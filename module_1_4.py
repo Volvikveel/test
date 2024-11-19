87add0e2568abc52bf1aa98f3a88e514bdc705da
Artur Khalikov <142807901+7etzkii@users.noreply.github.com>
2024-11-19 14:50:10 +0300

    first commit

---
+++ module_1_4.py
@@ -0,0 +1,9 @@
+#
+# my_string = input("Введите любое слово или фразу: ")
+# print(my_string. upper())
+# print(my_string. lower())
+# print(my_string. replace(" ", ""))
+# print(my_string[:1])
+# print(my_string[-1:])
+
+
